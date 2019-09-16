d6abf930bf30954dfbfe06cf4f989fe46c64a209
makansari78 <mak.ansari@rjtcompuquest.com>
2019-09-17 01:26:19 +0530

    Initial commit

---
+++ app/src/main/java/com/ansari/b21firstproj/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ansari.b21firstproj
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
